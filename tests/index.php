9541d04c62bb45539e22bdc288fc536a11eaa48b
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-27 18:21:12 +0200

    Update index.php

--- tests/index.php
+++ tests/index.php
@@ -1,9 +1,12 @@
 <?php
 
-/*
- * PHP-Base64 (https://github.com/delight-im/PHP-Base64)
- * Copyright (c) delight.im (https://www.delight.im/)
- * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+/**
+ * Pinga Base64
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on PHP-Base64 (https://github.com/delight-im/PHP-Base64) by delight.im (https://www.delight.im/)
+ *
+ * @license MIT
  */
 
 \error_reporting(\E_ALL);
@@ -17,21 +20,21 @@ function fail($lineNumber) {
 	exit('Error on line ' . $lineNumber);
 }
 
-(\Delight\Base64\Base64::encode('Gallia est omnis divisa in partes tres') === 'R2FsbGlhIGVzdCBvbW5pcyBkaXZpc2EgaW4gcGFydGVzIHRyZXM=') or \fail(__LINE__);
-(\Delight\Base64\Base64::decode('R2FsbGlhIGVzdCBvbW5pcyBkaXZpc2EgaW4gcGFydGVzIHRyZXM=') === 'Gallia est omnis divisa in partes tres') or \fail(__LINE__);
-(\Delight\Base64\Base64::encodeUrlSafe('πάντα χωρεῖ καὶ οὐδὲν μένει …') === 'z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg~~') or \fail(__LINE__);
-(\Delight\Base64\Base64::decodeUrlSafe('z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg~~') === 'πάντα χωρεῖ καὶ οὐδὲν μένει …') or \fail(__LINE__);
-(\Delight\Base64\Base64::encodeUrlSafeWithoutPadding('πάντα χωρεῖ καὶ οὐδὲν μένει …') === 'z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg') or \fail(__LINE__);
-(\Delight\Base64\Base64::decodeUrlSafeWithoutPadding('z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg') === 'πάντα χωρεῖ καὶ οὐδὲν μένει …') or \fail(__LINE__);
+(\Pinga\Base64\Base64::encode('Gallia est omnis divisa in partes tres') === 'R2FsbGlhIGVzdCBvbW5pcyBkaXZpc2EgaW4gcGFydGVzIHRyZXM=') or \fail(__LINE__);
+(\Pinga\Base64\Base64::decode('R2FsbGlhIGVzdCBvbW5pcyBkaXZpc2EgaW4gcGFydGVzIHRyZXM=') === 'Gallia est omnis divisa in partes tres') or \fail(__LINE__);
+(\Pinga\Base64\Base64::encodeUrlSafe('πάντα χωρεῖ καὶ οὐδὲν μένει …') === 'z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg~~') or \fail(__LINE__);
+(\Pinga\Base64\Base64::decodeUrlSafe('z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg~~') === 'πάντα χωρεῖ καὶ οὐδὲν μένει …') or \fail(__LINE__);
+(\Pinga\Base64\Base64::encodeUrlSafeWithoutPadding('πάντα χωρεῖ καὶ οὐδὲν μένει …') === 'z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg') or \fail(__LINE__);
+(\Pinga\Base64\Base64::decodeUrlSafeWithoutPadding('z4DOrM69z4TOsSDPh8-Jz4HOteG_liDOus6x4b22IM6_4b2QzrThvbLOvSDOvM6tzr3Otc65IOKApg') === 'πάντα χωρεῖ καὶ οὐδὲν μένει …') or \fail(__LINE__);
 
 for ($i = 0; $i < 1000; $i++) {
 	$data = \openssl_random_pseudo_bytes(
 		\mt_rand(12, 24)
 	);
 
-	(\Delight\Base64\Base64::decode(\Delight\Base64\Base64::encode($data)) === $data) or \fail(__LINE__);
-	(\Delight\Base64\Base64::decodeUrlSafe(\Delight\Base64\Base64::encodeUrlSafe($data)) === $data) or \fail(__LINE__);
-	(\Delight\Base64\Base64::decodeUrlSafeWithoutPadding(\Delight\Base64\Base64::encodeUrlSafeWithoutPadding($data)) === $data) or \fail(__LINE__);
+	(\Pinga\Base64\Base64::decode(\Pinga\Base64\Base64::encode($data)) === $data) or \fail(__LINE__);
+	(\Pinga\Base64\Base64::decodeUrlSafe(\Pinga\Base64\Base64::encodeUrlSafe($data)) === $data) or \fail(__LINE__);
+	(\Pinga\Base64\Base64::decodeUrlSafeWithoutPadding(\Pinga\Base64\Base64::encodeUrlSafeWithoutPadding($data)) === $data) or \fail(__LINE__);
 }
 
 echo 'ALL TESTS PASSED' . "\n";
