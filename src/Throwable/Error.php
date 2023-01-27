08f65e76837f784b746d327c564fb66cd88b2332
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-27 18:20:11 +0200

    Update Error.php

--- src/Throwable/Error.php
+++ src/Throwable/Error.php
@@ -1,12 +1,15 @@
 <?php
 
-/*
- * PHP-Base64 (https://github.com/delight-im/PHP-Base64)
- * Copyright (c) delight.im (https://www.delight.im/)
- * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+/**
+ * Pinga Base64
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on PHP-Base64 (https://github.com/delight-im/PHP-Base64) by delight.im (https://www.delight.im/)
+ *
+ * @license MIT
  */
 
-namespace Delight\Base64\Throwable;
+namespace Pinga\Base64\Throwable;
 
 /** Base class for all (unchecked) errors */
 abstract class Error extends \Exception {}
